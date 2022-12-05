6b97809aefc25567dfc1fae153507f4a07ab6aa1
mskk3215 <m.kaaiweb@gmail.com>
2022-12-05 23:05:40 +0900

    login condition add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Home from "./components/Home";
 
 function App() {
+  const [loggedInStatus, setLoggedInStatus] = useState("未ログイン");
+  // const [user, setUser] = useState("");
+
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route
+            path={"/"}
+            render={(props) => (
+              <Home {...props} loggedInStatus={loggedInStatus} />
+            )}
+          />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Registration from "./auth/Registration";
 
-export default function Home() {
+export default function Home(props) {
   return (
     <>
       <h1>Home</h1>
+      <h2>ログイン状態:{props.loggedInStatus}</h2>
       <Registration />
     </>
   );
