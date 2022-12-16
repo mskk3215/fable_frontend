10130072554d17c81fc2290373cd5ff860c659e5
mskk3215 <m.kaaiweb@gmail.com>
2022-12-16 22:43:10 +0900

    check login status add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Home from "./components/Home";
+import { logged_inUrl } from "./urls";
 
 function App() {
   const [loggedInStatus, setLoggedInStatus] = useState("未ログイン");
   const [user, setUser] = useState({});
+
   const handleLogin = (data) => {
     setLoggedInStatus("ログインnow");
     setUser(data.user);
+  };
+
+  useEffect(() => {
+    checkLoginStatus();
+  });
+
+  const checkLoginStatus = () => {
+    axios
+      .get(logged_inUrl, { withCredentials: true })
+      .then((response) => {
+        if (response.data.logged_in && loggedInStatus === "未ログイン") {
+          setLoggedInStatus("ログインnow");
+          setUser(response.data.user);
+        } else if (
+          !response.data.logged_in &&
+          loggedInStatus === "ログインnow"
+        ) {
+          setLoggedInStatus("未ログイン");
+          setUser({});
+        }
+      })
+      .catch((error) => {
+        console.log("ログインエラー", error);
+      });
   };
 
   return (
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Login from "./auth/Login";
 import Registration from "./auth/Registration";
 
 export default function Home(props) {
@@ -8,6 +9,7 @@ export default function Home(props) {
     props.handleLogin(data);
     navigate("/dashboard");
   };
+
   return (
     <>
       <h1>Home</h1>
@@ -15,6 +17,7 @@ export default function Home(props) {
       <Registration
         handleSuccessfulAuthentication={handleSuccessfulAuthentication}
       />
+      <Login handleSuccessfulAuthentication={handleSuccessfulAuthentication} />
     </>
   );
 }
