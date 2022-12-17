232ba8c8dc72b25fe609d8036ee8257f47ca18d5
mskk3215 <m.kaaiweb@gmail.com>
2022-12-17 20:18:37 +0900

    fix:refactoring user management

--- src/urls/index.js
+++ src/urls/index.js
@@ -3,3 +3,4 @@ const DEFAULT_API_LOCALHOST = "http://localhost:3001/api/v1";
 export const registrationUrl = `${DEFAULT_API_LOCALHOST}/signup`;
 export const loginUrl = `${DEFAULT_API_LOCALHOST}/login`;
 export const logoutUrl = `${DEFAULT_API_LOCALHOST}/logout`;
+export const logged_inUrl = `${DEFAULT_API_LOCALHOST}/logged_in`;
