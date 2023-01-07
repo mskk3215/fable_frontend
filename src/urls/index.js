cc8eb9cecb561d660922cb315c027dba18f2c840
mskk3215 <m.kaaiweb@gmail.com>
2023-01-07 15:23:25 +0900

    post api add

--- src/urls/index.js
+++ src/urls/index.js
@@ -1,6 +1,37 @@
-const DEFAULT_API_LOCALHOST = "http://localhost:3001/api/v1";
+import axios from "axios";
 
+//default_api
+export const DEFAULT_API_LOCALHOST = "http://localhost:3001/api/v1";
+
+//ユーザー管理
 export const registrationUrl = `${DEFAULT_API_LOCALHOST}/signup`;
 export const loginUrl = `${DEFAULT_API_LOCALHOST}/login`;
 export const logoutUrl = `${DEFAULT_API_LOCALHOST}/logout`;
 export const logged_inUrl = `${DEFAULT_API_LOCALHOST}/logged_in`;
+
+//post
+export const postClient = axios.create({
+  baseURL: DEFAULT_API_LOCALHOST,
+  headers: {
+    "Content-type": "multipart/form-data",
+  },
+});
+
+export const getPosts = () => {
+  return postClient.get("/posts");
+};
+export const createPosts = (data) => {
+  return postClient.post("/posts", data);
+};
+export const deletePosts = (id) => {
+  return postClient.delete(`/posts/${id}`);
+};
+
+postClient.interceptors.request.use((request) => {
+  console.log(request);
+  return request;
+});
+postClient.interceptors.response.use((response) => {
+  console.log(response);
+  return response;
+});
