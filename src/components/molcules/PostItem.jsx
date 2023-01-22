e277cf3f2a78ff2172f21900bb66544e177b241d
mskk3215 <m.kaaiweb@gmail.com>
2023-01-23 00:11:29 +0900

    post item modify for adding text

--- src/components/molcules/PostItem.jsx
+++ src/components/molcules/PostItem.jsx
@@ -1,4 +1,5 @@
-import { Card, CardMedia } from "@mui/material";
+import { Card, CardMedia, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import { deletePosts } from "../../urls";
 
 export const PostItem = ({ post, handleGetPosts }) => {
@@ -12,7 +13,22 @@ export const PostItem = ({ post, handleGetPosts }) => {
     <>
       <Card>
         {post.image?.url ? (
-          <CardMedia component="img" src={post.image.url} alt="post img" />
+          <Box sx={{ position: "relative" }}>
+            <CardMedia component="img" src={post.image.url} alt="post img" />
+            <Box
+              sx={{
+                position: "absolute",
+                bottom: 0,
+                left: 0,
+                width: "100%",
+                color: "white",
+                padding: "10px",
+              }}
+            >
+              <Typography variant="body2">昆虫名</Typography>
+              <Typography variant="body2">公園名</Typography>
+            </Box>
+          </Box>
         ) : null}
       </Card>
     </>
