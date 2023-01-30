f81b825b52d324868909030683b166f0d65cb9b9
mskk3215 <m.kaaiweb@gmail.com>
2023-01-30 23:59:34 +0900

    refactoring:post item

--- src/components/molcules/PostItem.jsx
+++ src/components/molcules/PostItem.jsx
@@ -1,41 +1,31 @@
-import { Card, CardMedia, Typography } from "@mui/material";
-import { Box } from "@mui/system";
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 import styled from "styled-components";
 import { deletePosts } from "../../urls";
 
-export const PostItem = ({ post, handleGetPosts }) => {
+export const PostItem = (props) => {
+  const { post, handleGetPosts } = props;
+
   const handleDeletePost = async (id) => {
     await deletePosts(id).then(() => {
       handleGetPosts();
     });
   };
 
-  const CustomBox = styled(Box)({
+  const CustomCard = styled(Card)({
     "&:hover": { opacity: 0.7 },
   });
 
   return (
     <>
-      <Card>
-        {post.image?.url ? (
-          <CustomBox sx={{ position: "relative" }}>
-            <CardMedia component="img" src={post.image.url} alt="post img" />
-            <Box
-              sx={{
-                position: "absolute",
-                bottom: 0,
-                left: 0,
-                width: "100%",
-                color: "white",
-                padding: "10px",
-              }}
-            >
-              <Typography variant="body2">昆虫名</Typography>
-              <Typography variant="body2">公園名</Typography>
-            </Box>
-          </CustomBox>
-        ) : null}
-      </Card>
+      {post.image?.url ? (
+        <CustomCard>
+          <CardMedia component="img" src={post.image.url} alt={post.id} />
+          <CardContent>
+            <Typography variant="body2">昆虫名</Typography>
+            <Typography variant="body2">公園名</Typography>
+          </CardContent>
+        </CustomCard>
+      ) : null}
     </>
   );
 };
