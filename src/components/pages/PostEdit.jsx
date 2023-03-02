4c5dffef87afc84a6b3949de27d484a9148ebf31
mskk3215 <m.kaaiweb@gmail.com>
2023-03-02 23:47:22 +0900

    multiple check add  in post edit

--- src/components/pages/PostEdit.jsx
+++ src/components/pages/PostEdit.jsx
@@ -1,5 +1,5 @@
 import { Grid } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { PostItem } from "../molcules/PostItem";
 import { EditForm } from "../organisms/EditForm";
 import { useAllImages } from "../../hooks/useAllImages";
@@ -7,6 +7,49 @@ import { useAllImages } from "../../hooks/useAllImages";
 export default function PostEdit() {
   const { posts } = useAllImages();
   const [selectedIds, setSelectedIds] = useState([]);
+  const [selectedIndexes, setSelectedIndexes] = useState([]);
+  const [isShiftDown, setIsShiftDown] = useState(false);
+
+  const keydownHandler = (e) => {
+    if (e.key === "Shift") {
+      setIsShiftDown(true);
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("keydown", keydownHandler);
+    return () => {
+      document.removeEventListener("keydown", keydownHandler);
+    };
+  }, []);
+
+  const keyupHandler = (e) => {
+    if (e.key === "Shift") {
+      setIsShiftDown(false);
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("keyup", keyupHandler);
+    return () => {
+      document.removeEventListener("keyup", keyupHandler);
+    };
+  }, []);
+
+  const idToIndex = (id) => {
+    for (let i = 0; i < posts.length; i++) {
+      if (posts[i].id === id) {
+        return i;
+      }
+    }
+  };
+  const fill = (a, b) => {
+    const array = [];
+    const start = Math.min(a, b);
+    const last = Math.max(a, b);
+    for (let i = start + 1; i <= last; i++) {
+      array.push(i);
+    }
+    return array;
+  };
 
   return (
     <>
@@ -18,12 +61,27 @@ export default function PostEdit() {
                 key={post.id}
                 post={post}
                 handleSelect={() => {
-                  setSelectedIds((ids) => [...ids, post.id]);
+                  if (isShiftDown) {
+                    const lastSelectedIndex =
+                      selectedIndexes[selectedIndexes.length - 1];
+                    const selectedIndex = idToIndex(post.id);
+                    setSelectedIndexes((indexes) => [
+                      ...indexes,
+                      ...fill(selectedIndex, lastSelectedIndex),
+                    ]);
+                  } else {
+                    setSelectedIndexes((indexes) => [
+                      ...indexes,
+                      idToIndex(post.id),
+                    ]);
+                  }
                 }}
                 handleRemove={() => {
-                  setSelectedIds((ids) => ids.filter((id) => id !== post.id));
+                  setSelectedIndexes((indexes) =>
+                    indexes.filter((index) => index !== idToIndex(post.id))
+                  );
                 }}
-                checked={selectedIds.includes(post.id)}
+                checked={selectedIndexes.includes(idToIndex(post.id))}
                 isCheckboxVisible={true}
               />
             );
