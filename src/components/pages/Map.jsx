2f406d36e390da966a77f4938a4ece89c16ffa34
mskk3215 <m.kaaiweb@gmail.com>
2023-03-13 00:12:32 +0900

    Marker add in map

--- src/components/pages/Map.jsx
+++ src/components/pages/Map.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 export const Map = () => {
   const mapStyle = {
@@ -11,13 +11,30 @@ export const Map = () => {
     lng: 139.77521,
   };
 
+  const locations = [
+    {
+      name: "高尾山",
+      location: {
+        lat: 35.625412,
+        lng: 139.243739,
+      },
+    },
+    {
+      name: "昭和記念公園",
+      location: {
+        lat: 35.710975,
+        lng: 139.393889,
+      },
+    },
+  ];
+
   return (
     <LoadScript googleMapsApiKey="">
-      <GoogleMap
-        mapContainerStyle={mapStyle}
-        zoom={13}
-        center={defaultCenter}
-      ></GoogleMap>
+      <GoogleMap mapContainerStyle={mapStyle} zoom={13} center={defaultCenter}>
+        {locations?.map((item) => {
+          return <Marker key={item.name} position={item.location} />;
+        })}
+      </GoogleMap>
     </LoadScript>
   );
 };
