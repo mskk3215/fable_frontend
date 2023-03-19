361630e02bb4685d9dde6b10c5a8f3323ea700c5
mskk3215 <m.kaaiweb@gmail.com>
2023-03-19 12:49:50 +0900

    map page resized

--- src/components/pages/Map.jsx
+++ src/components/pages/Map.jsx
@@ -1,11 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 export const Map = () => {
-  const mapStyle = {
-    width: "800px",
-    height: "800px",
-  };
+  const [mapStyle, setMapStyle] = useState({});
   const defaultCenter = {
     lat: 35.69575,
     lng: 139.77521,
@@ -27,6 +24,19 @@ export const Map = () => {
       },
     },
   ];
+
+  //mapのサイズを動的に合わせる
+  useEffect(() => {
+    const handleResize = () => {
+      setMapStyle({
+        width: window.innerWidth + "px",
+        height: window.innerHeight + "px",
+      });
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <LoadScript googleMapsApiKey={process.env.REACT_APP_API_KEY}>
