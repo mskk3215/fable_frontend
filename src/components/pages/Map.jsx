1b9a9a6e754fdbc27c883b1096eacb2164446281
mskk3215 <m.kaaiweb@gmail.com>
2023-04-09 18:03:40 +0900

    marker location get from search result in Map

--- src/components/pages/Map.jsx
+++ src/components/pages/Map.jsx
@@ -1,30 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import { MapDrawer } from "../organisms/MapDrawer";
+import { SearchParkContext } from "../../providers/SearchParkProvider";
 
 export const Map = () => {
+  const { searchResults } = useContext(SearchParkContext);
   const [mapStyle, setMapStyle] = useState({});
   const defaultCenter = {
     lat: 35.69575,
     lng: 139.77521,
   };
 
-  const locations = [
-    {
-      name: "高尾山",
-      location: {
-        lat: 35.625412,
-        lng: 139.243739,
-      },
-    },
-    {
-      name: "昭和記念公園",
-      location: {
-        lat: 35.710975,
-        lng: 139.393889,
-      },
-    },
-  ];
+  const locations = searchResults.map((result) => {
+    const title = result.name;
+    const latLng = { lat: result.latitude, lng: result.longitude };
+    console.log(result);
+    return { title, latLng };
+  });
+
   const mapOptions = {
     mapTypeControl: false,
   };
@@ -60,7 +53,7 @@ export const Map = () => {
         options={mapOptions}
       >
         {locations?.map((item) => {
-          return <Marker key={item.name} position={item.location} />;
+          return <Marker key={item.title} position={item.latLng} />;
         })}
       </GoogleMap>
     </>
