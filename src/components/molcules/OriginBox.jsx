45af947f9e6dec2175987068c17c485f509d8ea6
mskk3215 <m.kaaiweb@gmail.com>
2023-04-27 22:54:18 +0900

    originbox modify to add cancel button

--- src/components/molcules/OriginBox.jsx
+++ src/components/molcules/OriginBox.jsx
@@ -1,9 +1,14 @@
 import * as React from "react";
 import { Autocomplete } from "@react-google-maps/api";
-import { TextField } from "@mui/material";
+import { IconButton, TextField } from "@mui/material";
+import Close from "@mui/icons-material/Close";
 
 export const OriginBox = (props) => {
-  const { originRef, address } = props;
+  const { originRef, address, setAddress } = props;
+
+  const handleDeleteClick = () => {
+    setAddress(undefined);
+  };
 
   return (
     <Autocomplete>
@@ -12,6 +17,17 @@ export const OriginBox = (props) => {
         value={address === null ? undefined : address}
         sx={{ width: 300, height: 50 }}
         inputRef={originRef}
+        InputProps={{
+          endAdornment: (
+            <React.Fragment>
+              {address !== undefined && (
+                <IconButton onClick={handleDeleteClick}>
+                  <Close />
+                </IconButton>
+              )}
+            </React.Fragment>
+          ),
+        }}
       />
     </Autocomplete>
   );
