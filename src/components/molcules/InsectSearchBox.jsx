2b3afc5ac1803298bee0986dbb3f42496352cb5a
mskk3215 <m.kaaiweb@gmail.com>
2023-05-01 17:02:59 +0900

    fix:display logic for DirectionsIcon

--- src/components/molcules/InsectSearchBox.jsx
+++ src/components/molcules/InsectSearchBox.jsx
@@ -90,7 +90,7 @@ export const InsectSearchBox = (props) => {
                   ) : (
                     ""
                   )}
-                  {selectedItemId ? (
+                  {selectedItemId >= 1 ? (
                     <IconButton onClick={handleDirectionButtonClick}>
                       <DirectionsIcon color="primary" />
                     </IconButton>
