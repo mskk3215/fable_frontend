55037ea75aec5344fecf45aa5784fca89ed945d4
mskk3215 <m.kaaiweb@gmail.com>
2023-05-27 23:35:59 +0900

    fix: Divider add to "MapDrawer" again

--- src/components/organisms/MapDrawer.jsx
+++ src/components/organisms/MapDrawer.jsx
@@ -135,6 +135,7 @@ export const MapDrawer = (props) => {
                 </React.Fragment>
               ))
             )}
+            <Divider />
           </List>
         </Box>
       </Drawer>
