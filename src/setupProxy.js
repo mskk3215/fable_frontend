84a27a7ef08cc0edacf2db9663fbdeddc0c7395f
mskk3215 <m.kaaiweb@gmail.com>
2023-07-22 18:07:07 +0900

    setupProxy modified

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,6 @@
-import { Application } from "express";
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function (app: Application) {
+module.exports = function (app) {
   app.use(
     "/api/v1",
     createProxyMiddleware({
