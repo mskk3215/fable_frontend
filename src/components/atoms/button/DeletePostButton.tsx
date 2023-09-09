fe1f98920673b72869ff1f3eb3e59bb883ca8b65
mskk3215 <m.kaaiweb@gmail.com>
2023-09-09 10:58:21 +0900

    fix:error handling in "DeletePostButton" improved

--- src/components/atoms/button/DeletePostButton.tsx
+++ src/components/atoms/button/DeletePostButton.tsx
@@ -1,4 +1,6 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
+import { useSetRecoilState } from "recoil";
+import { messageState } from "../../../store/atoms/errorAtom";
 import { deletePosts } from "../../../urls";
 import { Button } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
@@ -10,9 +12,31 @@ type Props = {
 export const DeletePostButton = memo((props: Props) => {
   const { postId } = props;
 
+  const setMessage = useSetRecoilState(messageState);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   const handleDeleteButtonClick = async (postId: number) => {
+    setIsLoading(true);
+
     //サーバーに削除する投稿を送信
-    await deletePosts(postId);
+    await deletePosts(postId)
+      .then((response) => {
+        if (response.data.status === "deleted") {
+          setMessage("削除しました");
+        }
+      })
+      .catch((error) => {
+        if (!error.response || error.response.status >= 500) {
+          setMessage(
+            "ネットワークエラーが発生しました。しばらくしてから再試行してください。"
+          );
+        } else {
+          setMessage(error.response.data.error_messages);
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
 
   return (
@@ -23,6 +47,7 @@ export const DeletePostButton = memo((props: Props) => {
         variant={"outlined"}
         color="secondary"
         size="small"
+        disabled={isLoading}
       >
         {"削除する"}
       </Button>
