2a63f4c6caa8f444348e4cafb7e3c8ba1b6588ea
mskk3215 <m.kaaiweb@gmail.com>
2023-11-03 19:39:34 +0900

    feat:type of “useStasticsMap"

---
+++ src/types/statistics.ts
@@ -0,0 +1,21 @@
+export type GeoJSONFeature = {
+  type: string;
+  properties: {
+    name: string;
+    N03_001: string;
+    N03_003: string | null;
+    N03_004: string | null;
+  };
+  geometry: {
+    type: string;
+    coordinates: number[][][];
+  };
+};
+
+export type GeoJSONFeatureCollection = {
+  features: GeoJSONFeature[];
+};
+
+export type PrefectureCoordinates = {
+  [key: string]: number[];
+};
