f8df11d4bd0c1be23a2d66e3002f03ce418629b5
mskk3215 <m.kaaiweb@gmail.com>
2023-11-03 19:38:45 +0900

    fix:Header's Statistics link path

--- src/components/pages/Statistics.tsx
+++ src/components/pages/Statistics.tsx
@@ -1,5 +1,28 @@
+// Statistics.tsx
 import React from "react";
+import { StatisticsMap } from "../organisms/StatisticsMap";
+import { CorrectionStatusTable } from "../organisms/CorrectionStatusTable";
+import { CorrectionDoughnutChart } from "../organisms/CorrectionDoughnutChart";
+import { CorrectionRankingChart } from "../organisms/CorrectionRankingChart";
+import { Container } from "@mui/material";
 
 export const Statistics = () => {
-  return <></>;
+  return (
+    <Container
+      disableGutters
+      maxWidth="md"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <StatisticsMap />
+      <CorrectionDoughnutChart />
+      <CorrectionRankingChart />
+      <CorrectionStatusTable isCorrected={false} />
+      <CorrectionStatusTable isCorrected={true} />
+    </Container>
+  );
 };
