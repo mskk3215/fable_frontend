8deea1c55af3702391d1a17a1a0dc263853aa8a2
mskk3215 <m.kaaiweb@gmail.com>
2023-11-25 15:54:45 +0900

    fix:start location icon displayed in “Direction"

--- src/components/organisms/MapView.tsx
+++ src/components/organisms/MapView.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useEffect } from "react";
+import React, { memo, useEffect, useState } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import {
   DirectionsRenderer,
@@ -7,14 +7,18 @@ import {
   MarkerF,
   useLoadScript,
 } from "@react-google-maps/api";
+import { getGeocode, getLatLng } from "use-places-autocomplete";
 import ReactHtmlParser from "react-html-parser";
 import {
   mapApiLoadState,
   selectedCenterState,
   selectedItemState,
 } from "../../store/atoms/MapDirectionState";
-import { destinationLocationState } from "../../store/atoms/searchWordState";
-import { Location } from "../../types/map";
+import {
+  destinationLocationState,
+  originLocationState,
+} from "../../store/atoms/searchWordState";
+import { LatLng, Location } from "../../types/map";
 import { Park } from "../../types/parks";
 
 type Props = {
@@ -43,8 +47,13 @@ export const MapView = memo((props: Props) => {
     useRecoilState(selectedCenterState);
   const [selectedItemId, setSelectedItemId] = useRecoilState(selectedItemState);
   const [mapLoadState, setMapLoadState] = useRecoilState(mapApiLoadState);
+  const originLocation = useRecoilValue(originLocationState);
   const destinationLocation = useRecoilValue(destinationLocationState);
 
+  const [startLatLng, setStartLatLng] = useState<LatLng>({
+    lat: 0,
+    lng: 0,
+  });
   const mapOptions = {
     fullscreenControl: false,
     mapTypeControl: false,
@@ -56,6 +65,16 @@ export const MapView = memo((props: Props) => {
     const latLng = { lat: result.latitude, lng: result.longitude };
     return { id, title, latLng };
   });
+
+  //出発地点の緯度経度を取得する
+  useEffect(() => {
+    if (!mapLoadState.isLoaded || mapLoadState.loadError || !originLocation)
+      return;
+    getGeocode({ address: originLocation })
+      .then((results) => getLatLng(results[0]))
+      .then((latLng) => setStartLatLng(latLng))
+      .catch((error) => console.error("Error: ", error));
+  }, [originLocation, mapLoadState]);
 
   //Google Maps APIの読み込み状態を管理する
   const { isLoaded, loadError } = useLoadScript({
@@ -70,12 +89,18 @@ export const MapView = memo((props: Props) => {
   if (!mapLoadState.isLoaded) return "Loading Maps";
 
   //Iconの設定
-  const renderIcon = (id: number) => {
+  const renderIcon = (id?: number) => {
     const selectedIconUrl = process.env.PUBLIC_URL + "/images/selectedIcon.png";
     const parkIconUrl = process.env.PUBLIC_URL + "/images/parkIcon.png";
-
+    const currentLocationIconUrl =
+      process.env.PUBLIC_URL + "/images/currentLocationIcon.png";
     return {
-      url: id === selectedItemId ? selectedIconUrl : parkIconUrl,
+      url:
+        id !== undefined
+          ? id === selectedItemId
+            ? selectedIconUrl
+            : parkIconUrl
+          : currentLocationIconUrl,
       size: new window.google.maps.Size(50, 50),
       origin: new window.google.maps.Point(0, 0),
       anchor: new window.google.maps.Point(25, 25),
@@ -129,6 +154,19 @@ export const MapView = memo((props: Props) => {
             />
           )
       )}
+      {isDirectionPage &&
+        originLocation &&
+        startLatLng.lat &&
+        startLatLng.lng && (
+          <MarkerF
+            position={startLatLng}
+            options={{
+              icon: renderIcon(),
+              animation: window.google.maps.Animation.DROP,
+            }}
+            title="出発地点"
+          ></MarkerF>
+        )}
       {directions && (
         <DirectionsRenderer
           directions={directions}
@@ -142,12 +180,6 @@ export const MapView = memo((props: Props) => {
           }}
           onLoad={onLoadHook}
         />
-      )}
-      {directions?.routes[0]?.legs[0]?.start_location && (
-        <MarkerF
-          position={directions.routes[0].legs[0].start_location}
-          title="出発地点"
-        ></MarkerF>
       )}
       {!isDirectionLoading && infoWindowLocation && (
         <InfoWindowF
