ee6abcc183955c49962f960b89854e0fc1f12c4d
mskk3215 <m.kaaiweb@gmail.com>
2023-12-03 17:30:01 +0900

    fix:usePageSize updated to extend selection of pageSize

--- src/hooks/usePageSize.ts
+++ src/hooks/usePageSize.ts
@@ -3,17 +3,23 @@ import { useMediaQuery, useTheme } from "@mui/material";
 export const usePageSize = () => {
   // 画面サイズによって表示する画像の数を変更する
   const theme = useTheme();
+  const isXS = useMediaQuery(theme.breakpoints.down("sm"));
   const isSM = useMediaQuery(theme.breakpoints.down("md"));
   const isMD = useMediaQuery(theme.breakpoints.between("md", "lg"));
-  const isLG = useMediaQuery(theme.breakpoints.up("lg"));
+  const isLG = useMediaQuery(theme.breakpoints.between("lg", "xl"));
+  const isXL = useMediaQuery(theme.breakpoints.up("xl"));
 
   let pageSize = 20;
-  if (isSM) {
+  if (isXS) {
+    pageSize = 6;
+  } else if (isSM) {
     pageSize = 8;
   } else if (isMD) {
     pageSize = 12;
   } else if (isLG) {
     pageSize = 15;
+  } else if (isXL) {
+    pageSize = 20;
   }
   return pageSize;
 };
