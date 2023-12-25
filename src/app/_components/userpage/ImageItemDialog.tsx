b06ecd6c5a1d83ba735787b032d74bb894f1ba29
mskk3215 <m.kaaiweb@gmail.com>
2023-12-25 23:08:36 +0900

    fix:ImageItemDialog modified for leaking open state

--- src/app/_components/userpage/ImageItemDialog.tsx
+++ src/app/_components/userpage/ImageItemDialog.tsx
@@ -64,6 +64,7 @@ export const ImageItemDialog = memo((props: Props) => {
   return (
     <>
       <Dialog
+        open={imageOpen}
         onClose={handleClickImageClose}
         fullWidth={true}
         maxWidth={"xl"}
