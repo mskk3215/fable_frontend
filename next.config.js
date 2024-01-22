f86cbc6419db1430692930121321ca2568e551fd
mskk3215 <m.kaaiweb@gmail.com>
2024-01-22 23:08:58 +0900

    fix:next.config modified to resolve connection bug to backend

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 module.exports = {
   images: {
-    domains: ["localhost"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        // hostname: "localhost",
+        hostname: "host.docker.internal",
+        // production環境で最適化されないバグががある
+        // https://github.com/vercel/next.js/issues/58248
+      },
+    ],
   },
   compiler: {
     styledComponents: true,
   },
+  output: "standalone",
 };
