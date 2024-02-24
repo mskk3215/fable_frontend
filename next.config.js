15ab5e0ca1eb425cf3a6973f07b2a144efbc75b7
mskk3215 <m.kaaiweb@gmail.com>
2024-02-24 14:26:36 +0900

    fix:bug where next/image wasn't displaying in production by correcting hostname in next.config.js resolved

--- next.config.js
+++ next.config.js
@@ -2,11 +2,9 @@ module.exports = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        // hostname: "localhost",
-        hostname: "host.docker.internal",
-        // production環境で最適化されないバグががある
-        // https://github.com/vercel/next.js/issues/58248
+        protocol: "https",
+        hostname: "*.cloudfront.net",
+        pathname: "/uploads/**",
       },
     ],
   },
