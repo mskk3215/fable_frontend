6934e0e8a904d37603b73854d8ded473204dfe1a
mskk3215 <m.kaaiweb@gmail.com>
2024-04-09 22:10:09 +0900

    feat:Login test add

--- src/app/_components/auth/__mock__/msw.ts
+++ src/app/_components/auth/__mock__/msw.ts
@@ -9,6 +9,13 @@ type RegistrationBody = {
   };
 };
 
+type LoginBody = {
+  session: {
+    email?: string;
+    password: string;
+  };
+};
+
 // 新規登録処理のモック
 export const handleRegistration = () => {
   return rest.post("/api/v1/users", async (req, res, ctx) => {
@@ -38,3 +45,28 @@ export const handleRegistration = () => {
   });
 };
 
+// ログイン処理のモック
+export const handleLogin = () => {
+  return rest.post("/api/v1/login", async (req, res, ctx) => {
+    const body: LoginBody = await req.json();
+    // 失敗時
+    if (body.session.email === "401@example.com") {
+      return res(
+        ctx.status(401),
+        ctx.json({
+          errors: [
+            "ログインに失敗しました。",
+            "入力した情報を確認して再度お試しください。",
+          ],
+        })
+      );
+    }
+    // 成功時
+    return res(
+      ctx.json({
+        loggedIn: true,
+        user: { id: 1, nickname: "Ares", email: "Ares@example.com" },
+      })
+    );
+  });
+};
