76ef078bb5a26708c69c247952e41f8ef8d1f881
mskk3215 <m.kaaiweb@gmail.com>
2024-08-12 14:18:33 +0900

    feat:SightingNotificationModal add

--- src/app/_components/usernotificationlist/UserNotificationList.tsx
+++ src/app/_components/usernotificationlist/UserNotificationList.tsx
@@ -3,9 +3,15 @@
 import React, { useCallback, useEffect, useState } from "react";
 import Link from "next/link";
 import { useInsectSightingNotifications } from "../../../hooks/useSightingNotifications";
+import { useRecoilValue } from "recoil";
 import styled from "styled-components";
 import { throttle } from "lodash";
 import { SightingNotifications } from "../../../types/sightingnotifications";
+import { deleteUserSightingNotification } from "../../../urls";
+import {
+  notificationSettingState,
+  sightingNotificationState,
+} from "../../../store/atoms/notificationAtom";
 import {
   Box,
   List,
@@ -24,7 +30,10 @@ export const UserNotificationList = () => {
     isSightingNotificationInitialLoading,
     isSightingNotificationLoading,
     setSightingNotificationPage,
+    handleGetSightingNotifications,
   } = useInsectSightingNotifications();
+  const sightingNotifications = useRecoilValue(sightingNotificationState);
+  const notificationSetting = useRecoilValue(notificationSettingState);
 
   // scrollで投稿を追加取得
   const handleSightingNotificationScroll = throttle(() => {
@@ -43,30 +52,69 @@ export const UserNotificationList = () => {
       window.removeEventListener("scroll", handleSightingNotificationScroll);
   }, []);
 
+  const [notificationOpen, setNotificationOpen] = useState(false);
+
+  // Modal open
+  const handleNotificationModalOpen = useCallback(() => {
+    setNotificationOpen(true);
+  }, []);
+
+  // Modal close
+  const handleNotificationModalClose = useCallback(async () => {
+    setNotificationOpen(false);
+    // 通知設定の変更をサーバーに送信。notificationSettingでfalseになっているものだけを送信
+    const deletePromises = sightingNotifications
+      .filter(
+        (notification: SightingNotifications) =>
+          !notificationSetting[notification.insectId]
+      )
+      .map((notification) => {
+        deleteUserSightingNotification(notification.id);
+      });
+    await Promise.all(deletePromises);
+    handleGetSightingNotifications(undefined);
+    handleGetSightingNotifications(undefined, true);
+  }, [notificationSetting]);
+
   return (
     <>
       <Box sx={{ width: "98%", m: "auto", marginTop: "70px", maxWidth: 600 }}>
         <Typography variant="h4" sx={{ mb: 2 }}>
           昆虫の出没情報一覧
         </Typography>
+        <UserNotificationModal
+          userSightingNotifications={userSightingNotifications}
+          notificationOpen={notificationOpen}
+          handleNotificationModalOpen={handleNotificationModalOpen}
+          handleNotificationModalClose={handleNotificationModalClose}
+        />
         <List>
-          {isSightingNotificationInitialLoading
-            ? Array.from(new Array(8)).map((_, index) => (
-                <SkeltonStyledListItem key={index}>
-                  <ListItemText
-                    primary={<Skeleton variant="text" height={100} />}
-                  />
-                </SkeltonStyledListItem>
-              ))
-            : userSightingNotifications?.map(
-                (notification: SightingNotifications, index) => (
-                  <SightingNotificationList
-                    key={index}
-                    notification={notification}
-                    index={index}
-                  />
-                )
-              )}
+          {isSightingNotificationInitialLoading ? (
+            Array.from(new Array(8)).map((_, index) => (
+              <SkeltonStyledListItem key={index}>
+                <ListItemText
+                  primary={<Skeleton variant="text" height={100} />}
+                />
+              </SkeltonStyledListItem>
+            ))
+          ) : userSightingNotifications.length > 0 ? (
+            userSightingNotifications.map(
+              (notification: SightingNotifications, index) => (
+                <SightingNotificationList
+                  key={index}
+                  notification={notification}
+                  index={index}
+                />
+              )
+            )
+          ) : (
+            <Box>
+              <Typography color="error.main">通知がありません。</Typography>
+              <Typography color="error.main">
+                通知設定が無い場合、通知設定を追加してください
+              </Typography>
+            </Box>
+          )}
         </List>
       </Box>
       {isSightingNotificationLoading &&
---
+++ src/app/_components/usernotificationlist/UserNotificationModal.tsx
@@ -0,0 +1,131 @@
+"use client";
+
+import React, { memo } from "react";
+import { useRecoilValue } from "recoil";
+import Link from "next/link";
+import { sightingNotificationState } from "../../../store/atoms/notificationAtom";
+import { SightingNotificationButton } from "../SightingNotificationButton";
+import {
+  Box,
+  Button,
+  ButtonBase,
+  IconButton,
+  Modal,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { Cancel } from "@mui/icons-material";
+import { SightingNotifications } from "../../../types/sightingnotifications";
+
+type Props = {
+  userSightingNotifications?: SightingNotifications[];
+  notificationOpen: boolean;
+  handleNotificationModalOpen: () => void;
+  handleNotificationModalClose: () => void;
+};
+
+export const UserNotificationModal = memo((props: Props) => {
+  const {
+    notificationOpen,
+    handleNotificationModalOpen,
+    handleNotificationModalClose,
+  } = props;
+  const sightingNotifications = useRecoilValue(sightingNotificationState);
+
+  return (
+    <>
+      <Box sx={{ display: "flex", justifyContent: "flex-end", mb: 2 }}>
+        <Button
+          onClick={handleNotificationModalOpen}
+          variant="contained"
+          sx={{
+            backgroundColor: "#2b3d51",
+            color: "#ffffff",
+            "&:hover": {
+              backgroundColor: "#2b3d51",
+              opacity: 0.7,
+            },
+            mr: 1.5,
+          }}
+        >
+          通知設定
+        </Button>
+        <Modal open={notificationOpen} onClose={handleNotificationModalClose}>
+          <Box
+            sx={{
+              m: "auto",
+              bgcolor: "background.paper",
+              borderRadius: "10px",
+              outline: "none",
+              width: "90%",
+              maxWidth: 400,
+              marginTop: 10,
+              position: "relative",
+            }}
+          >
+            <Typography
+              variant="h5"
+              sx={{
+                p: 2,
+                boxShadow: "0 3px 6px rgba(0,0,0,0.1)",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              通知設定一覧
+            </Typography>
+            <IconButton
+              sx={{ position: "absolute", top: 14, right: 10 }}
+              onClick={handleNotificationModalClose}
+            >
+              <Cancel />
+            </IconButton>
+            <Box sx={{ overflowY: "scroll", maxHeight: "70vh", p: 3 }}>
+              <Stack spacing={3}>
+                {sightingNotifications?.map((notification, index) => (
+                  <React.Fragment key={index}>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <Box sx={{ display: "flex", alignItems: "center" }}>
+                        <ButtonBase
+                          component={Link}
+                          href={`/picturebook/${notification.insectId}`}
+                        >
+                          <Typography
+                            sx={{
+                              cursor: "pointer",
+                              textDecoration: "none",
+                              "&:hover": {
+                                textDecoration: "underline",
+                              },
+                              marginRight: 1,
+                            }}
+                          >
+                            {notification.insectName}
+                          </Typography>
+                        </ButtonBase>
+                      </Box>
+                      <SightingNotificationButton
+                        insectId={notification.insectId}
+                      />
+                      {/* <NotificationListButton
+                        insectId={notification.insectId}
+                      /> */}
+                    </Box>
+                  </React.Fragment>
+                ))}
+              </Stack>
+            </Box>
+          </Box>
+        </Modal>
+      </Box>
+    </>
+  );
+});
+UserNotificationModal.displayName = "UserNotificationModal";
